f12ee62c25fa5f6e61ce0dd3901d28f2d8fa7c2c
Chris Anatalio <anataliocs@gmail.com>
2016-01-19 16:09:42 -0500

    Add expected args to data commands and updated basic cmd intrepreter

--- src/database/cmd/interpreter/BasicCmdInterpreter.java
+++ src/database/cmd/interpreter/BasicCmdInterpreter.java
@@ -13,6 +13,9 @@ import java.util.stream.Collectors;
 public class BasicCmdInterpreter implements CmdInterpreter {
     private static final String DELIMITER = " ";
 
+    private static final int FIRST_ARG = 1;
+    private static final int SECOND_ARG = 2;
+
     private static final String INVALID_NUM_OF_ARGS = "Invalid # of arguments";
     private static final String INVALID_CMD = "Invalid Command";
 
@@ -28,9 +31,12 @@ public class BasicCmdInterpreter implements CmdInterpreter {
                 .filter(rc -> rc != null && !rc.isEmpty())
                 .collect(Collectors.toList());
 
+        return buildCommand(args);
+    }
+
+    private Cmd buildCommand(List<String> args) {
         final DataCommand command = DataCommand.getCommandFromType(args.get(0));
 
-        //create command
         if (command != null) {
             switch (command) {
                 case END:
@@ -42,29 +48,30 @@ public class BasicCmdInterpreter implements CmdInterpreter {
                 case ROLLBACK:
                     return new RollbackCmd();
                 case SET:
-                    if (args.size() == 3) {
-                        return new SetCmd(args.get(1), args.get(2));
+                    if (args.size() == DataCommand.SET.getRequiredNumOfArgs()) {
+                        return new SetCmd(args.get(FIRST_ARG), args.get(SECOND_ARG));
                     }
                     return new InvalidCmd(INVALID_NUM_OF_ARGS);
                 case GET:
-                    if (args.size() == 2) {
-                        return new GetCmd(args.get(1));
+                    if (args.size() == DataCommand.GET.getRequiredNumOfArgs()) {
+                        return new GetCmd(args.get(FIRST_ARG));
                     }
                     return new InvalidCmd(INVALID_NUM_OF_ARGS);
                 case NUMEQUALTO:
-                    if (args.size() == 2) {
-                        return new NumEqualToCmd(args.get(1));
+                    if (args.size() == DataCommand.NUMEQUALTO.getRequiredNumOfArgs()) {
+                        return new NumEqualToCmd(args.get(FIRST_ARG));
                     }
                     return new InvalidCmd(INVALID_NUM_OF_ARGS);
                 case UNSET:
-                    if (args.size() == 2) {
-                        return new UnsetCmd(args.get(1));
+                    if (args.size() == DataCommand.UNSET.getRequiredNumOfArgs()) {
+                        return new UnsetCmd(args.get(FIRST_ARG));
                     }
                     return new InvalidCmd(INVALID_NUM_OF_ARGS);
                 default:
                     break;
             }
         }
+
         return new InvalidCmd(INVALID_CMD);
     }
 }
--- src/database/model/DataCommand.java
+++ src/database/model/DataCommand.java
@@ -3,20 +3,23 @@ package database.model;
 import java.util.Arrays;
 
 public enum DataCommand {
-    SET("SET"),
-    UNSET("UNSET"),
-    GET("GET"),
-    NUMEQUALTO("NUMEQUALTO"),
-    END("END"),
-    BEGIN("BEGIN"),
-    COMMIT("COMMIT"),
-    ROLLBACK("ROLLBACK"),
-    INVALID("INVALID");
+    SET("SET",3),
+    UNSET("UNSET",2),
+    GET("GET",2),
+    NUMEQUALTO("NUMEQUALTO",2),
+    END("END",0),
+    BEGIN("BEGIN",0),
+    COMMIT("COMMIT",0),
+    ROLLBACK("ROLLBACK",0),
+    INVALID("INVALID",0);
 
     private final String command;
+    private final int requiredNumOfArgs;
 
-    private DataCommand(String command) {
+    private DataCommand(String command, int args) {
+
         this.command = command;
+        this.requiredNumOfArgs = args;
     }
 
     public static DataCommand getCommandFromType(final String type) {
@@ -27,5 +30,9 @@ public enum DataCommand {
 
     String getCommand() {
         return command;
+    }
+
+    public int getRequiredNumOfArgs() {
+        return requiredNumOfArgs;
     }
 }
